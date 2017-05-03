b3b227ad04bd3ee41cb6f026084980a125b479c5
Felix Favre <felix.favre@protonet.info>
2017-05-03 15:51:13 +0200

    change entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
+chown platform:platform /files -Rc
 
 find /files/ -type f -exec chmod 660 -c {} +
 find /files/ -type d -exec chmod 770 -c {} +
-find /files/ -type f -user root -exec chown platform:platform -c {} +
-find /files// -type d -user root -exec chown platform:platform -c {} +
