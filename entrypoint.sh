81d3a2652c78a33e175e195c7cb997d50ee8e0c9
Felix Favre <felix.favre@protonet.info>
2017-05-03 15:46:47 +0200

    add chmod commands to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,7 @@
 #!/bin/sh
 
-echo "Test"
 
-mount
-
-ls /
-
-ls /files/
-echo `ls /files/`
-
-cat /files/test
-
-echo "Test 2"
+find /files/ -type f -exec chmod 660 -c {} +
+find /files/ -type d -exec chmod 770 -c {} +
+find /files/ -type f -user root -exec chown platform:platform -c {} +
+find /files// -type d -user root -exec chown platform:platform -c {} +
